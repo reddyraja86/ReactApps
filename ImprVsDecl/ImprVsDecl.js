750695ad8fc2e17d784db3d496bf07c95f802b48
Raju Rayappa <rrayappa@opentext.com>
2022-02-23 21:59:04 +0530

    Imperative Vs Declarative Approach

---
+++ ImprVsDecl/ImprVsDecl.js
@@ -0,0 +1,12 @@
+
+//Delarative approach
+ReactDOM.render(<h1>Hi Declarative way</h1>, document.getElementById("root"))
+
+//Imperative waof adding a h1 Element
+/**
+var elemt = document.createElement("h1");
+elemt.innerText = "Created using the imperative Programming";
+
+var root = document.getElementById("root");
+root.append(elemt);
+ */
